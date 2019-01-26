2a9c0af9d71a0ae3bfaf02ef59c4a3b3dab35648
Deore <=>
2019-01-26 17:35:29 +0530

    first commit

---
+++ hello-web/src/main/java/com/cb/helloweb/HelloWebApplication.java
@@ -0,0 +1,25 @@
+package com.cb.helloweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class HelloWebApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWebApplication.class, args);
+	}
+	
+	
+	@Bean
+	public RestTemplate getTemplate() {
+		return new RestTemplate();
+	}
+
+}
+
